59124562e12fe6f09aadd90d773f7d80b2440284
kmansfield <kim@kimansfield.com>
2017-12-21 13:34:59 -0700

    This is the first check in of this project.

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * This is to test the submodules capabilities of git
+ */
+
+#include "../subTest/openWrap.h"
+#include <stdio.h>
+
+int main(int argc,char *argv[])
+{
+    int fd;
+    int len;
+    char buf[256];
+
+    fd = wOpen("test");
+    if (fd > 0)
+    {
+        printf("success we opened it\n");
+        len = read(fd,buf,80);
+        printf("buf = %s\n",buf);
+        fd = wClose(fd);
+    }
+    else
+        printf("We bombe\n");
+}
